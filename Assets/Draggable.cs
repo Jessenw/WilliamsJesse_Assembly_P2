234a8637712a8658b50a10cd3cc99d68de6927ba
Guest User <Guest@wg401-h-02-07.mddn.vuw.ac.nz>
2018-08-02 10:45:59 +1200

    Added item toolbar. Item cards return to bar and align properly

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	
+	Transform itemBar = null;
 
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag");
+		
+		itemBar = this.transform.parent;
+		this.transform.SetParent(this.transform.parent.parent);
     }
 
     public void OnDrag(PointerEventData eventData) {
@@ -17,5 +22,7 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("OnEndDrag");
+		
+		this.transform.SetParent(itemBar);
     }
 }

8f5e67afdf72abe215d8232c3df00d0577118aca
Jesse Williams <jessenw3@gmail.com>
2018-08-09 23:53:44 +1200

    #3 - Added comments to placement script

--- assembly-game/Assets/Scripts/PlaceScript.cs
+++ assembly-game/Assets/Scripts/PlaceScript.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * This script allows an item to follow the mouse with grid snapping. If the player clicks
+ * the left mouse button, a new item finalObject is created at the grid position.
+ * This implementation is sourced from: https://www.youtube.com/watch?v=D9ZU0mfukQE
+ */
 public class PlaceScript : MonoBehaviour {
 
     [SerializeField]
-    private GameObject finalObject;
+    private GameObject finalObject; // The object that will be placed
 
     private Vector2 mousePos;
 	
@@ -13,10 +18,13 @@ public class PlaceScript : MonoBehaviour {
 	void Update ()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        /*  Round the position so that is snaps to the grid and assign to position */
         transform.position = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
 
-        if (Input.GetMouseButtonDown(0))
+        /* If the left mouse button is pressed, instantiate the finalObject */
+        if (Input.GetMouseButtonDown(0)) // 0 = left mouse button
         {
+            /* Object to instantiate, position of finalObject, rotation of finalObject */
             Instantiate(finalObject, transform.position, Quaternion.identity);
         }
 	}

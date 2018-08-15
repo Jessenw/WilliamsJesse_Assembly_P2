21872f230fab144633bf2a41c89d9221e42c90f0
Guest User <Guest@wg401-g-05-06.mddn.vuw.ac.nz>
2018-08-16 09:52:11 +1200

    #3 implemented basic moving from mouse onto plane

---
+++ assembly-3d/Assets/Scripts/PlacementController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementController : MonoBehaviour 
+{
+
+    [SerializeField]
+    private GameObject placeable;
+
+    [SerializeField]
+    private KeyCode hotkey = KeyCode.A;
+
+    private GameObject currentPlaceable;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        HandleInput();
+
+        if (currentPlaceable != null)
+        {
+            MoveToMouse();
+        }
+	}
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(hotkey))
+        {
+            if (currentPlaceable == null)
+            {
+                currentPlaceable = Instantiate(placeable);
+            }
+            else
+            {
+                Destroy(currentPlaceable);
+            }
+        }
+    }
+
+    private void MoveToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            currentPlaceable.transform.position = hit.point;
+        }
+    }
+}

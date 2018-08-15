78dd64436911490e4bafe54833148dd63ad42a11
Jesse Williams <jessenw3@gmail.com>
2018-08-16 07:32:38 +1200

    Fixes #4 Implemented conveyor belts

---
+++ assembly-3d/Assets/Scripts/AcceptItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcceptItem : MonoBehaviour {
+
+    public GameObject machine;
+
+    void OnTriggerStay(Collider item)
+    {
+        Debug.Log("Collision");
+        Destroy(item);
+    }
+}
---
+++ assembly-3d/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour {
+
+    public GameObject belt;
+    public Transform endpoint;
+    public float speed;
+
+    void OnTriggerStay(Collider item)
+    {
+        item.transform.position = Vector3.MoveTowards(item.transform.position, endpoint.position, speed * Time.deltaTime);
+    }
+}

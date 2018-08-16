934f42f4a5e7b870b4a16cfdac51be0048aecceb
Guest User <jessenw3@gmail.com>
2018-08-16 12:29:02 +1200

    final commit before leaving lab

--- assembly-3d/Assets/Scripts/AcceptItem.cs
+++ assembly-3d/Assets/Scripts/AcceptItem.cs
@@ -6,9 +6,15 @@ public class AcceptItem : MonoBehaviour {
 
     public GameObject machine;
 
+    // Keep a count of how many items there are
+    private int itemCount1 = 0;
+    private int itemCount2 = 0;
+
     void OnTriggerEnter(Collider item)
     {
         Debug.Log("Collision");
         Destroy(item.gameObject);
+
+        itemCount1++;
     }
 }
---
+++ assembly-3d/Assets/Scripts/SpawnItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItem : MonoBehaviour 
+{
+
+    [SerializeField]
+    public GameObject item; // The item to be spawn
+
+    private float timer = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > 3)
+        {
+            Vector3 checkBoxCenter = new Vector3(transform.position.x, transform.position.y + 0.3f, transform.position.z);
+            Vector3 checkBox = new Vector3(0.2f, 0.2f, 0.2f);
+            if (Physics.CheckBox(checkBoxCenter, checkBox))
+            {
+                Debug.Log("Contains");
+            }
+            else
+            {
+                Instantiate(item, transform.position, transform.rotation);
+            }
+            timer = 0f;
+        }
+	}
+}

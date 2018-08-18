dff8b8d45f7266501b391df949d8ad114333aa44
Jesse Williams <jessenw3@gmail.com>
2018-08-18 18:43:07 +1200

    #5 engine recipe implemented

--- assembly-3d/Assets/Scripts/AcceptItem.cs
+++ assembly-3d/Assets/Scripts/AcceptItem.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class AcceptItem : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject engine;
+
     // Keep a count of how many items there are
     private int itemCount1 = 0;
     private int itemCount2 = 0;
+
+    private float timer = 2.0f;
+
+    void Update()
+    {
+        if ((itemCount2 >= 2) && (itemCount1 >= 1))
+        {
+            if (timer < 0)
+            {
+                Vector3 spawnPos = new Vector3(transform.position.x + 1.5f, transform.position.y + 1.0f, transform.position.z);
+                Instantiate(engine, spawnPos, transform.rotation);
+
+                itemCount2 -= 2;
+                itemCount1--;
+
+                timer = 2.0f;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+        }
+    }
 
     void OnTriggerEnter(Collider item)
     {
@@ -16,6 +42,14 @@ public class AcceptItem : MonoBehaviour {
             Destroy(item.gameObject);
             itemCount1++;
             Debug.Log("Copper detected | count " + itemCount1);
+        }
+
+        Steel steel = item.gameObject.GetComponent<Steel>();
+        if (steel != null)
+        {
+            Destroy(item.gameObject);
+            itemCount2++;
+            Debug.Log("Steel detected | count " + itemCount2);
         }
     }
 }

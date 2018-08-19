8cb322351640b2e7c93ff5388eee3bb90bc0e3c6
Jessenw <Guest@wg401-h-07-07.mddn.vuw.ac.nz>
2018-08-19 15:54:22 +1200

    Began working on carriage script

---
+++ assembly-3d/Assets/Scripts/Carriage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carriage : MonoBehaviour 
+{
+    Random rnd = new Random();
+
+	// Use this for initialization
+	void Start () 
+    {
+		// Generate a random item 
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+}
---
+++ assembly-3d/Assets/Scripts/Controllers/TrainController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainController : MonoBehaviour 
+{
+    /* Location of where the train will stop */
+    [SerializeField]
+    public Transform trainStop;
+
+    /* How long until the train enters the scene */
+    private float entryTimer = 2.0f;
+
+    /* How long until the train leaves */
+    private float waitTimer = 15.0f;
+
+    private float timer = 2.0f;
+    private float speed = 2.0f;
+
+    // Update is called once per frame
+    void Update () 
+    {
+		if (timer < 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, trainStop.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+	}
+}

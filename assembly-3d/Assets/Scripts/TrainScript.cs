deff0709a5bf5fbff8ff4ce6d55aa9cd741ce019
Guest User <Guest@wg401-g-05-06.mddn.vuw.ac.nz>
2018-08-20 15:19:19 +1200

    Added text to train

--- assembly-3d/Assets/Scripts/TrainScript.cs
+++ assembly-3d/Assets/Scripts/TrainScript.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class TrainScript : MonoBehaviour 
 {
     
-    string[] inputItems; // The items that the train requires
+    string[] requiredItems; // The items that the train requires
     int   [] itemNeeded;  // The needed amount of each item
 
 	void Start () 
     {
-        int itemCount = Random.Range(0, 3); // Generate number of items
-        inputItems = new string[itemCount];
-
+        int itemCount = Random.Range(1, 2); // Generate number of items
+        requiredItems = new string[itemCount];
 
         for (int i = 0; i < itemCount; i++)
         {
@@ -20,16 +19,18 @@ public class TrainScript : MonoBehaviour
             int requiredAmount = 0;        // How much of the item is needed
 
             /* Generate the item */
-            int randomItem = Random.Range(0, 5);
+            int randomItem = Random.Range(0, 4);
             if (randomItem == 0) item = "circuit";
             else if (randomItem == 1) item = "engine";
             else if (randomItem == 2) item = "glass";
             else if (randomItem == 3) item = "wheel";
-            inputItems[i] = item;
+            requiredItems[i] = item;
+            Debug.Log("Item: " + item);
 
             /* Generate the amount required */
-            requiredAmount = Random.Range(0, 5);
-            requiredAmount *= 50;
+            requiredAmount = Random.Range(1, 5);
+            requiredAmount *= 10;
+            Debug.Log("Item amount: " + requiredAmount);
         }
 	}
 	
@@ -39,9 +40,18 @@ public class TrainScript : MonoBehaviour
 		
 	}
 
-    private void OnTriggerEnter(Collider obj)
+    private void OnTriggerEnter(Collider col)
     {
-        Destroy(collider.gameObject);
-
+        GameObject gameObj = col.gameObject;
+        if (gameObj.GetComponent<Engine>() != null && requiredItems[0] == "engine")
+        {
+            Destroy(gameObj);
+            itemNeeded[0]--;
+        }
+        else if (gameObj.GetComponent<Circuit>() != null && requiredItems[0] == "circuit")
+        {
+            Destroy(gameObj);
+            itemNeeded[0]--;
+        }
     }
 }

73c2187944a30f9380ab2b9b971eb6610ac6d012
Guest User <Guest@wg401-j-02-04.mddn.vuw.ac.nz>
2018-08-20 15:58:02 +1200

    Train displays required item and item count

--- assembly-3d/Assets/Scripts/TrainScript.cs
+++ assembly-3d/Assets/Scripts/TrainScript.cs
@@ -1,17 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrainScript : MonoBehaviour 
 {
+    [SerializeField]
+    public Text uiText;
+
+    [SerializeField]
+    public Transform offScreenTrainStop;
+
+    public float offScreenTimer = 10.0f;
+
+    [SerializeField]
+    public float speed = 2.0f;
     
     string[] requiredItems; // The items that the train requires
-    int   [] itemNeeded;  // The needed amount of each item
+    int[] itemNeeded;  // The needed amount of each item
 
 	void Start () 
     {
+        NewTrain();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        int itemCount = itemNeeded[0];
+        string item = requiredItems[0];
+        uiText.text = item + " [" + itemCount + "]";
+
+        if (itemCount <= 0)
+        {
+            NextTrain();
+        }
+	}
+
+    void NewTrain()
+    {
         int itemCount = Random.Range(1, 2); // Generate number of items
         requiredItems = new string[itemCount];
+        itemNeeded = new int[itemCount];
 
         for (int i = 0; i < itemCount; i++)
         {
@@ -20,30 +50,41 @@ public class TrainScript : MonoBehaviour
 
             /* Generate the item */
             int randomItem = Random.Range(0, 4);
+            /* Debug */
+            randomItem = 1;
             if (randomItem == 0) item = "circuit";
             else if (randomItem == 1) item = "engine";
             else if (randomItem == 2) item = "glass";
             else if (randomItem == 3) item = "wheel";
             requiredItems[i] = item;
-            Debug.Log("Item: " + item);
 
             /* Generate the amount required */
             requiredAmount = Random.Range(1, 5);
             requiredAmount *= 10;
-            Debug.Log("Item amount: " + requiredAmount);
+            /* Debug */
+            requiredAmount = 3;
+            itemNeeded[0] = requiredAmount;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    }
+
+    void NextTrain()
     {
-		
-	}
+        if (offScreenTimer <= 0)
+        {
+            NewTrain();
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, offScreenTrainStop.position, speed * Time.deltaTime);
+        }
+    }
 
     private void OnTriggerEnter(Collider col)
     {
+        Debug.Log("monkey");
+
         GameObject gameObj = col.gameObject;
-        if (gameObj.GetComponent<Engine>() != null && requiredItems[0] == "engine")
+        if ((gameObj.GetComponent<Engine>() != null) && (requiredItems[0] == "engine"))
         {
             Destroy(gameObj);
             itemNeeded[0]--;

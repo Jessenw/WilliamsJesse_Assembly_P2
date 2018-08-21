d93a65b477c3a4a7a463e989045c08dfdfc4c42f
Guest User <jessenw3@gmail.com>
2018-08-21 15:40:18 +1200

    Fixes #18 timer implemented. game ends on expire

---
+++ assembly-3d/Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* Ends the current game if the level timer expires */
+public class GameOver : MonoBehaviour 
+{
+	void Update () 
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
---
+++ assembly-3d/Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour 
+{
+    [SerializeField]
+    public Text uiText;
+
+    public float timer = 0.0f;
+    private bool setGameOverCanvas = true;
+
+    private void Start()
+    {
+        timer = 120.0f;
+    }
+
+    void Update () 
+    {
+        int roundedTimer = Mathf.RoundToInt(timer);
+        uiText.text = "Time left: " + roundedTimer.ToString() + " seconds";
+        timer -= Time.deltaTime;
+
+        GameObject timerCanvas = transform.Find("TimerCanvas").gameObject;
+        GameObject uiTimer = timerCanvas.transform.Find("Timer").gameObject;
+
+        if (timer <= 0 && setGameOverCanvas)
+        {
+            timer = 0;
+            GameObject gameOverCanvas = transform.Find("GameOverCanvas").gameObject;
+            gameOverCanvas.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+	}
+}
